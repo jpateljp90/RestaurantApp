3a98ecbb7ecc02e28601ce1e45f3c939ddf1870d
Jay Patel <jpatel.jp90@gmail.com>
2024-02-18 14:40:51 -0500

    Initial commit(Implementation Splash screen)

---
+++ app/src/main/java/com/example/restaurantapp/WelcomeSplash.java
@@ -0,0 +1,18 @@
+package com.example.restaurantapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.restaurantapp.R;
+
+public class WelcomeSplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_splash);
+
+
+    }
+}
